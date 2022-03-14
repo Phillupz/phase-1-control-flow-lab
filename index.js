7fcae6ad404413a6a39da132f568a882b8e6783e
Phillupz <phillup.zukowski@gmail.com>
2022-03-14 15:27:32 -0400

    commit tree 92a0a88295822a8a9d620e4d09f3b0c521d42e63

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 199){
+  return ("This one is on me!")
+  }
+    else if (ride <= 2001) {
+      return ("I will gladly take your thirty bucks.")
+    }
+
+    else if (ride >= 2501){
+      return ("No can do.")
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function switchOnCharmFromTip(tip){
+    switch(tip){
+      case "generous":
+        return "Thank you so much."
+          break;
+        case "not as generous":
+        return "Thank you."
+          break;
+        default: 
+          return "Bye."
+
+
+    }
+  }
+
